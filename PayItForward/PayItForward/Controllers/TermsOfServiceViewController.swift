dd095e8617b2fdb3f7b87d3f1aa76aa3cb2a6409
Johnny Abercrombie <johnnyabercrombie9@gmail.com>
2017-05-07 18:22:00 -0700

    added TOS controller

---
+++ PayItForward/PayItForward/Controllers/TermsOfServiceViewController.swift
@@ -0,0 +1,43 @@
+//
+//  TermsOfServiceViewController.swift
+//  PayItForward
+//
+//  Created by Jonathan Abercrombie on 5/7/17.
+//  Copyright © 2017 PayItFoward. All rights reserved.
+//
+
+import UIKit
+
+class TermsOfServiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func doNotAccept(_ sender: UIButton) {
+    }
+    
+    @IBAction func accept(_ sender: UIButton) {
+        performSegue(withIdentifier: "toCreateAccount", sender: nil)
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
