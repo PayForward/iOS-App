21ca2407d8151d89d64aae84670cd7d11b816d92
Sarthak Khillon <sarthakkhillon24@gmail.com>
2017-05-14 04:11:07 -0700

    Added address finding functionality

--- PayItForward/PayItForward/Controllers/AddressViewController.swift
+++ PayItForward/PayItForward/Controllers/AddressViewController.swift
@@ -7,38 +7,100 @@
 //
 
 import UIKit
+import CoreLocation
+import FirebaseDatabase
 
 class AddressViewController: UIViewController {
-
+    
     @IBOutlet weak var addressField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var currentLocButton: UIButton!
+    var locationManager:CLLocationManager?
+    var currentLocation:CLLocation?
+    var usedLocationFinder = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.startUpdatingLocation()
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager?.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.authorizationStatus() == .denied || CLLocationManager.authorizationStatus() == .restricted {
+            self.currentLocButton.isEnabled = false
+        }
         // TODO: add shadows
+        
     }
-
+    
     @IBAction func useCurrentLocation(_ sender: UIButton) {
+        CLGeocoder().reverseGeocodeLocation(self.currentLocation!) { (placemarks, error) in
+            if error != nil && (placemarks?.count)! > 0 {
+                print(error!)
+                return
+            }
+            
+            let placemark = (placemarks?.first)!
+            self.addressField.text = placemark.name ?? "\(self.currentLocation!.coordinate.latitude), \(self.currentLocation!.coordinate.longitude)"
+            self.usedLocationFinder = true
+            print("reverse location: \(self.addressField.text!)")
+        }
         
     }
     
     @IBAction func next(_ sender: UIButton) {
+        guard self.addressField.text != "" else {
+            print("address field blank")
+            return
+        }
+        
+        if !usedLocationFinder {
+            self.getLocation(from: self.addressField.text!)
+        }
+        
+        self.save(self.currentLocation!)
         performSegue(withIdentifier: "next", sender: nil)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func getLocation(from address: String) {
+        CLGeocoder().geocodeAddressString(address) { (placemarks, error) in
+            guard error != nil && (placemarks?.count)! > 0 else {
+                print(error!)
+                return
+            }
+            
+            let placemark = (placemarks?.first)!
+            self.currentLocation = placemark.location!
+            print(self.currentLocation)
+        }
     }
-    */
+    
+    func save(_ userLoc: CLLocation) {
+        print("saved")
+        //        let userRef = FIRDatabase.database().reference(withPath: "user")
+        //        let lat = userLoc.coordinate.latitude
+        //        let long = userLoc.coordinate.longitude
+        //
+        //        userRef.child("location").child("longitude").setValue(long)
+        //        userRef.child("location").child("latitude").setValue(lat)
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+}
 
+extension AddressViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations.first
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
 }
